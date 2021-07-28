7a464cd68a6ed0a4b74aa5b7e6a97c463d1856f1
whitesock23 <tiagomiguelseixas@me.com>
2021-07-28 21:40:11 +0100

    transition

--- README.md
+++
@@ -1,1 +0,0 @@
-# noa-assistant-server
--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    console.log("project-management-server JS imported successfully!");
-  },
-  false
-);
